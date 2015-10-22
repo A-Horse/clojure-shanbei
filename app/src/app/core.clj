afbfc521d4ca16242bcb83be1216d8ceb4f5f7ee
AbyChan <crystal.tyan@gmail.com>
2015-10-23 00:57:47 +0800

    update

--- app/src/app/core.clj
+++ app/src/app/core.clj
@@ -5,8 +5,7 @@
 
 (install-pretty-logging)
 
-(def cli-prompt "> "
-  "cli prompt")
+(def cli-prompt "➜")
 
 (def should-exit? false)
 
@@ -14,20 +13,22 @@
 
 (def exit-command ["exit" "q" "quit"])
 
-
-
 (def app-ascii "•?((¯°·._.• ȼℓ๏jµя€ $hąɲβą¥ •._.·°¯))؟•")
 
 (defn log-info []
-  (println str (bold ())))
+  (println (str (bold (green app-ascii)))))
+
+(defn print-prompt []
+  (println (str (bold (green cli-prompt)))))
 
 (defn -main 
   "main"
   [& args]
-  (println (str (bold (green "➜"))))
+  (println cli-prompt)
   (loop [input (read-line)]
     (if-not (some #(= input %) exit-command)
       (do (println input)
           (recur (read-line)))
       (do (println "EXIT")))))
+
 
